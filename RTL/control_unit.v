b628ff696ac799a26776c8ef0a475a02b8b672ab
Thibaut Smeets <r0755727@stud-r8-spice-8.esat.kuleuven.be>
2022-03-09 11:04:58 +0100

    Task 1 finished

--- RTL/control_unit.v
+++ RTL/control_unit.v
@@ -42,6 +42,61 @@ module control_unit(
             alu_op    = R_TYPE_OPCODE;
             jump      = 1'b0;
          end
+
+	ALU_I:begin
+            alu_src   = 1'b1;
+            mem_2_reg = 1'b0;
+            reg_write = 1'b1;
+            mem_read  = 1'b0;
+            mem_write = 1'b0;
+            branch    = 1'b0;
+            alu_op    = ADD_OPCODE;
+            jump      = 1'b0;
+         end
+
+	BRANCH_EQ:begin
+            alu_src   = 1'b0;
+            mem_2_reg = 1'b0;
+            reg_write = 1'b0;
+            mem_read  = 1'b0;
+            mem_write = 1'b0;
+            branch    = 1'b1;
+            alu_op    = SUB_OPCODE;
+            jump      = 1'b0;
+         end
+
+	JUMP:begin
+            alu_src   = 1'b0;
+            mem_2_reg = 1'b0;
+            reg_write = 1'b0;
+            mem_read  = 1'b0;
+            mem_write = 1'b0;
+            branch    = 1'b1;
+            alu_op    = R_TYPE_OPCODE;
+            jump      = 1'b1;
+         end
+
+	LOAD:begin
+            alu_src   = 1'b1;
+            mem_2_reg = 1'b1;
+            reg_write = 1'b1;
+            mem_read  = 1'b1;
+            mem_write = 1'b0;
+            branch    = 1'b0;
+            alu_op    = ADD_OPCODE;
+            jump      = 1'b0;
+         end
+
+	STORE:begin
+            alu_src   = 1'b1;
+            mem_2_reg = 1'b0;
+            reg_write = 1'b0;
+            mem_read  = 1'b0;
+            mem_write = 1'b1;
+            branch    = 1'b0;
+            alu_op    = ADD_OPCODE;
+            jump      = 1'b0;
+         end
          
          // Declare the control signals for each one of the instructions here...
 
